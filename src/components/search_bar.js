19ee09a033249d129bc788da43618b5e78f9d4a5
Jonny Moore <jonnymoore121212@gmail.com>
2017-05-01 21:41:23 +0100

    updates searchTerm as user types in search bar

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,5 +1,21 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const SearchBar = () => {
-  return <input />;
+class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { searchTerm: '' };
+  }
+
+  render() {
+    return (
+      <div>
+        <input
+          value={this.state.searchTerm}
+          onChange={event => this.setState({ searchTerm: event.target.value })}/>
+      </div>
+    );
+  }
 }
+
+export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SearchBar from './components/search_bar';
 
 const API_KEY = "AIzaSyAUSIbJx6kDp6wCGZtYR8sPXmPt2FKIllY";
 
 const App = () => {
-  return <div>Hi!</div>;
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
